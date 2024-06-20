f03f6b2923fc9b9c3cd957da42815338e10ce627
hanadasuka <hanadasuka@gmail.com>
2024-06-20 22:35:57 +0200

    Added more in Program.cs

--- Bokkingsystem/Program.cs
+++ Bokkingsystem/Program.cs
@@ -2,7 +2,11 @@
 using Bokkingsystem.Data;
 using Bokkingsystem.Services;
 using BookingModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace Bokkingsystem
 {
@@ -32,7 +36,7 @@ namespace Bokkingsystem
                 options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
             });
 
-            builder.Services.AddAuthorization();
+
 
             /*
             builder.Services.AddAuthentication(options =>
@@ -41,6 +45,74 @@ namespace Bokkingsystem
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             });
             */
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audiecne"],
+                        IssuerSigningKey = new SymmetricSecurityKey
+                        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                // This is to generate the Default UI of Swagger Documentation
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ASP.NET 8 WEB API",
+                    Description = "Authentication"
+                });
+                // To enable authorization using Swagger(JWT)
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            }
+                        }, Array.Empty<string>()
+                    }
+                });
+            });
+
+            // Policy 
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminCompanyUserPolicy", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireRole("admin", "company", "user");
+                });
+                options.AddPolicy("AdminCompanyPolicy", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireRole("admin", "company");
+                });
+                options.AddPolicy("AdminUserPolicy", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireRole("admin", "user");
+                });
+            });
 
 
 
